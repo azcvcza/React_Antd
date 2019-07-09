42f0b4ed9f8e16a2cdd8bd1f661d6785a561f6ae
韦粤信 <283940802@qq.com>
2019-07-10 02:49:02 +0800

    build new routes

--- app/config/config.js
+++ app/config/config.js
@@ -7,6 +7,14 @@ export default {
     ],
     routes: [{
         path: '/',
-        component: './HelloWorld',
-    }]
+        component: './layout',
+        routes: [{
+            path: '/dashboard',
+            routes: [
+                { path: '/dashboard/analysis', component: './Dashboard/Analysis' },
+                { path: '/dashboard/monitor', component: './Dashboard/Monitor' },
+                { path: '/dashboard/workplace', component: './Dashboard/Workplace' }
+            ]
+        }, ]
+    }],
 };
