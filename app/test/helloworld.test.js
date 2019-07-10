c0d0bb9b392b82672833a0936fefa2e975ae5321
韦粤信 <283940802@qq.com>
2019-07-10 15:18:13 +0800

    simply add test

---
+++ app/test/helloworld.test.js
@@ -0,0 +1,13 @@
+import { mount } from 'enzyme';
+import TestDemo from '../src/component/TestDemo'
+const sum = function(a, b) {
+    return a + b;
+}
+test('adds 1+2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+})
+
+test('TestDemo', () => {
+            const wrapper = mount( < TestDemo > < /TestDemo>);
+                expect(wrapper.find('div').text()).toBe('test');
+            })
